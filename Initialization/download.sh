c2a4744696bf6f2a2216229a5ba56d12146e3cc6
Sukulent <roma.pholcin@gmail.com>
2026-01-25 11:43:18 +0600

    chore: organizing folders

--- Initialization/download.sh
+++ Initialization/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S --needed swww zsh rofi waybar swaync alacritty socat jq nwg-look flameshot iwd impala bluez bluetui wiremix hypridle nautilus 
+sudo pacman -S --needed swww zsh rofi waybar swaync alacritty socat jq nwg-look flameshot iwd impala bluez bluetui wiremix hypridle nautilus exa
 
-yay -S 
+yay -S nautilus-open-any-terminal actions-for-nautilus-git
+
+./set-nautilus-terminal.sh #https://github.com/Stunkymonkey/nautilus-open-any-terminal
